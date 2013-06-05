ad0d9b62c0cff43fa6fec5e4710197c7f7a9d0be
joamag <joamag@gmail.com>
2013-06-05 10:15:37 +0100

    new header value

--- src/deployers/__init__.py
+++ src/deployers/__init__.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Hive Administration Scripts
+# Hive Remotia System
 # Copyright (c) 2008-2012 Hive Solutions Lda.
 #
-# This file is part of Hive Administration Scripts.
+# This file is part of Hive Remotia System.
 #
-# Hive Administration Scripts is free software: you can redistribute it and/or modify
+# Hive Remotia System is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hive Administration Scripts is distributed in the hope that it will be useful,
+# Hive Remotia System is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Hive Administration Scripts. If not, see <http://www.gnu.org/licenses/>.
+# along with Hive Remotia System. If not, see <http://www.gnu.org/licenses/>.
 
 __version__ = "1.0.0"
 """ The version of the module """
