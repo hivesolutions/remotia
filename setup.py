2f9d0b16aa328a5b24c2b6893470980ec5da9cd4
joamag <joamag@gmail.com>
2013-07-25 16:51:51 +0100

    new header values

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Hive Automium System
-# Copyright (C) 2008-2012 Hive Solutions Lda.
+# Hive Remotia System
+# Copyright (c) 2008-2012 Hive Solutions Lda.
 #
-# This file is part of Hive Automium System.
+# This file is part of Hive Remotia System.
 #
-# Hive Automium System is free software: you can redistribute it and/or modify
+# Hive Remotia System is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hive Automium System is distributed in the hope that it will be useful,
+# Hive Remotia System is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Hive Automium System. If not, see <http://www.gnu.org/licenses/>.
+# along with Hive Remotia System. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
