3f5eb2896f530068589ac43b2add0d919878c1da
joamag <joamag@gmail.com>
2013-09-25 17:55:48 +0100

    new version value

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ if not root_directory == "": os.chdir(root_directory)
 
 setuptools.setup(
     name = "remotia",
-    version = "0.1.5",
+    version = "0.1.6",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Remotia System",
