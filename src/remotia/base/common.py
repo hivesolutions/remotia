3e420bff86b34c19c5e8e70e7e0585b5a6634d70
joamag <joamag@gmail.com>
2013-12-20 19:05:27 +0000

    new common file

--- src/remotia/base/common.py
+++ src/remotia/base/common.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Remotia System. If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -34,8 +37,6 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import common
-import sched
-
-from common import *
-from sched import *
+VERSION = "0.1.8"
+""" The current version of the remotia infra-structure
+that may be used for logging and debugging purposes """
